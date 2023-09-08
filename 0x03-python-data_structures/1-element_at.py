725f77d0e0bf6c544b92f95d023f27ed3b041da6
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-08 14:52:34 +0300

    A  function that replaces an element of a list at a specific position (like in C).

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     count = len(my_list)
-    if idx < 0:
-        return None
-    i = 0
-    for num in my_list:
-        if i == idx:
-            return num
-        i += 1
+    if idx > 0 and idx < count:
+        return my_list[idx]
     return None
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    count = len(my_list)
+    if idx > 0 and idx < count:
+        my_list[idx] = element
+    return my_list
