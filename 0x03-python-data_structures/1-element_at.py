05689917af485e1c4a01eef7d45a4b7e2f6aa2cb
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-11 08:41:17 +0300

    Changes on 1-elem

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     count = len(my_list)
-    if idx > 0 and idx < count:
-        return my_list[idx]
-    return None
+    if idx < 0 and idx >= count:
+        return None
+    return my_list[idx]
