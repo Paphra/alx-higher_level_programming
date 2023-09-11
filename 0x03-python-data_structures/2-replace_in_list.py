5f559f2a70074ca34afa32e8ea1b4e3a7fe2d4aa
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-11 08:54:22 +0300

    Changes on 2-replace 003

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    lcp = my_list[:]
     if idx < 0 or idx >= len(my_list):
         return my_list
-    lcp[idx] = element
+    my_list[idx] = element
     return my_list
