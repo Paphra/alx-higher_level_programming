04c3c304ee5efe12fb9dc5f966b1e6932d90e5f1
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-13 14:40:31 +0300

    Hold on for 103 -- 001

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_python_list - prints the python list info
+ * @p: python object
+ * Return: nothing
+ */
+void print_python_list(PyObject *p)
+{
+
+}
+
+/**
+ * print_python_bytes - prints bytes about a python object
+ * @p: the object of python
+ * Return: nothing
+ */
+void print_python_bytes(PyObject *p)
+{
+}
