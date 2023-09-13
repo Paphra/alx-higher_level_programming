03d4ba6e47c09ebe9f5f6b7d9157b87f7008f086
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-13 13:24:59 +0300

    Hold on 10-best 003

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,14 +4,10 @@ def best_score(a_dictionary):
         return None
     score = 0
     key = ""
-    count = 0
     for k, v in a_dictionary.items():
-        if count == 0:
-            score = v
         if v > score:
             key = k
         score = v
-        count += 1
     if key == '':
         return None
     return key
