01f696c6783c4f61a05d269d8bd6639e4ae99c5f
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-25 04:39:12 -0700

    Edited #0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while (count < x):
             print("{}".format(my_list[count]), end='')
             count += 1
-    except:
+    except Exception:
         print()
     else:
         print()
