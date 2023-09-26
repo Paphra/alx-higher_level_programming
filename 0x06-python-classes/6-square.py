4f4d4920152c9cb8b5e38ee136a17cbb5329607d
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-26 03:23:06 -0700

    Changes correcting Task #6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -92,9 +92,13 @@ class Square:
 
         Set the current position to a value
         """
-
-        if len(value) != 2 or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+
         self.__position = value
 
     def my_print(self):
