12dfea73a9b575b8ad19a5978387fc77f6604d1c
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-09-30 11:17:15 -0700

    Some docs corrected

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,14 +8,9 @@ Contains a function for adding two integers
 
 def add_integer(a, b=98):
     """
-    Adds two integers
-
-    Args:
-        a (int): first integer or float
-        b (int): second integer or float, default=98
-    Returns: addition of the two
+    Returns addition of the two
     """
-    
+
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
