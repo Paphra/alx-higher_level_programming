77856a93aa7e9ddc937983c549b17d4d954a6292
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-10-02 15:13:06 -0700

    So many solves

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -11,6 +11,10 @@ import sys
 
 
 def solution():
+    """
+    Solution to the N-queens prob
+    """
+
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
@@ -25,14 +29,50 @@ def solution():
     if N < 4:
         print('N must be at least 4')
         sys.exit(1)
-    if N == 4:
-        print('[[0, 1], [1, 3], [2, 0], [3, 2]]')
-        print('[[0, 2], [1, 0], [2, 3], [3, 1]]')
-    elif N == 6:
-        print('[[0, 1], [1, 3], [2, 5], [3, 0], [4, 2], [5, 4]]')
-        print('[[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]')
-        print('[[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]')
-        print('[[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]')
+
+    board = [-1] * N
+    place_queens(board, 0)
+
+
+def place_queens(board, row):
+    """
+    Place the queens
+    """
+
+    N = len(board)
+
+    if row == N:
+        print_solution(board)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col):
+            board[row] = col
+            place_queens(board, row + 1)
+
+
+def is_safe(board, row, col):
+    """
+    Is it safe to print and fill row
+    """
+
+    for i in range(row):
+        if board[i] == col:
+            return False
+        if board[i] - i == col - row or board[i] + i == col + row:
+            return False
+    return True
+
+
+def print_solution(board):
+    """
+    Print the solution
+    """
+
+    sol = []
+    for i, col in enumerate(board):
+        sol.append([i, col])
+    print(sol)
 
 
 if __name__ == '__main__':
