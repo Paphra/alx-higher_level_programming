c64754971513e55deded6516e56de2d015b4d20d
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-10-03 20:17:07 -0700

    Changes 101

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Locked class module
+"""
+
+
+class LockedClass:
+    """Locked class
+    """
+
+    def __setattr__(self, name, value):
+        """ set attribute method
+
+        Only allow first_name
+        """
+
+        if name == 'first_name':
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError(
+                "'{}' object has no attribute '{}'".format(
+                    type(self).__name__, name
+                )
+            )
