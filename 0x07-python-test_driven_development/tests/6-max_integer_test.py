296fbd19b009a5316394c4ac218e9814c64d2055
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-10-03 19:57:07 -0700

    Chabges

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,8 +12,6 @@ class TestMaxIntegerFunction(unittest.TestCase):
     def test_empty_list(self):
         self.assertTrue(max_integer([]) is None)
 
-    def test
-
 
 if __name__ == '__main__':
     unittest.main()
