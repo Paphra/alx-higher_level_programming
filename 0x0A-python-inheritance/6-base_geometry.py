d1760b3c354bacae474ea8ce2b0e2b273dc64d4f
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-10-09 08:15:43 -0700

    A class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""6-base_geometry module
+
+Contains a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """Defines an instance method area
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
