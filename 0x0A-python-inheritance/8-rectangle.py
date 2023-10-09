a301dc51d669a604a7f99efa6b95033b1f0b0637
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-10-09 11:58:50 -0700

    Checkes08

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -23,7 +23,7 @@ class BaseGeometry:
         """Validates an integer
         """
 
-        if isinstance(value,  int):
+        if not isinstance(value,  int):
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
