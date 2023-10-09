f585f2fe803adb27ddb3fdd904a46b0dfae6db7a
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-10-09 12:59:38 -0700

    8 cjg

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -37,8 +37,8 @@ class Rectangle(BaseGeometry):
         """Initialization ofthe given values
         """
 
-        super().integer_validator('width', width)
-        super().integer_validator('height', height)
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
 
         self.__width = width
         self.__height = height
