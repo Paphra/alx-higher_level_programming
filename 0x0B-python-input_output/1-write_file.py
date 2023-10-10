88f2f79453871912fb537cf4e3e484c070f11621
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-10-10 15:18:03 -0700

    Changes

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,7 +7,6 @@ Contains a function that writes to a file
 
 def write_file(filename="", text=""):
     """Writes some given text to the file
-    
     Args:
         filename (str): string file name
         text (str): the text to write
