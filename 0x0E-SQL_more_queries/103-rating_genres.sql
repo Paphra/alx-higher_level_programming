767ab663861b4dbeaec6460c6df1a41eb42f4868
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-11-16 15:32:26 +0300

    Changes to last task

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- list all genres in the db by their rating
 SELECT
     tv_genres.name,
-    SUM(tv_show_ratings.rate) as rating
+    SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
