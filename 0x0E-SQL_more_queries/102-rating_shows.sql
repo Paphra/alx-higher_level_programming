8e8dae1920a0877e182ad8c9a16e2947d20fe05c
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-11-16 15:19:58 +0300

    a script that lists all shows from hbtn_0d_tvshows_rate by their rating.

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- list all shows from db by rating
+SELECT
+    tv_shows.title,
+    SUM(tv_show_ratings.rate) AS rating_sum
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating_sum DESC;
