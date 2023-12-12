3006298ae6c5fe9b41837277623185f9509fd21f
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-12 19:53:20 +0300

    Convert to bases

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,10 +2,6 @@
 
 exports.converter = function (base) {
   return function convert (number) {
-    if (number < base) {
-      return String(number);
-    } else {
-      return convert(Math.floor(number / base)) + String(number % base);
-    }
+    return number.toString(base);
   };
 };
