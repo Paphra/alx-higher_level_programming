12bc18c336554fa179457df0e011cb5ef689813c
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-16 12:18:37 +0300

    1- checks

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     )
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE 'n%' ORDER BY states.id ASC"
+    query = "SELECT * FROM states WHERE BINARY\
+        name LIKE 'N%' ORDER BY states.id ASC"
     cursor.execute(query)
     results = cursor.fetchall()
     for state in results:
