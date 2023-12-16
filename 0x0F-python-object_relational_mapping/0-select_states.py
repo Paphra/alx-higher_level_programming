cf64af3bfb6113a9ae24fb150933eee52c134955
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-16 10:27:55 +0300

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" The selcect_states module
+
+Contains the script to select all states from the database
+"""
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    host = 'localhost'
+    port = 3306
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(
+        host=host, port=port, user=user, password=password,
+        database=database
+    )
+    db.query("SELECT * FROM states ORDER BY states.id ASC")
+    result = db.store_result()
+    for state in result.fetch_row(maxrows=0):
+        print(state)
