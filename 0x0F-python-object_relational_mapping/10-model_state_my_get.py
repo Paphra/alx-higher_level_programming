bb62f3d82b70debbf73583297b332257b838b77a
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-17 06:19:33 +0300

    a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""model_state_my_get module
+Contains a script that prints the id of the searched State from the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def search_state(db_useranme, db_password, db, search):
+    """List all users
+    A function that prints all States
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            db_useranme, db_password, db
+        ), pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).filter(
+        State.name == search).first()
+    if (state is not None):
+        print(state.id)
+    else:
+        print('Not found')
+
+
+if __name__ == '__main__':
+    username, password, database, search = sys.argv[1:5]
+
+    search_state(username, password, database, search)
