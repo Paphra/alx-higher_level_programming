e61072d83086e5a422b8fdd9eb850ec8c0f8a528
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-05 20:41:46 +0300

    Changes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # A Script that sends a request and displays the size of the response body
-url="$1"
-curl -sI $url | grep 'Content-Length' | awk '{print $2}'
+curl -sI $1 | grep 'Content-Length' | awk '{print $2}'
