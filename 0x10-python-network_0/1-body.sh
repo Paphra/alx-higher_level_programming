9113cad0bb90f10dfa1607ad01368bd781fbd12b
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-05 21:22:15 +0300

    Changes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays its body if code is 200
-curl -s -X GET -w "%{http_code}" $1 | awk '$1 == "200" {print prev} {prev = $0}'
+curl -s -w "%{http_code}" $1 | awk '$1 == "200" {print prev} {prev = $0}'
