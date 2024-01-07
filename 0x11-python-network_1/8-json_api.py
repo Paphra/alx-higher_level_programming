77a70204a9105727c8097500aa0838a41670009c
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-07 22:34:26 +0300

    Changes

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,8 +21,8 @@ def do_request(url, letter):
     res = requests.post(url, data=data)
     try:
         json = res.json()
-        if len(json) > 0:
-            print('[{}] {}'.format())
+        if len(json.items) > 0:
+            print('[{}] {}'.format(json.get('id'), json.get('name')))
         else:
             print('No result')
     except ValueError:
