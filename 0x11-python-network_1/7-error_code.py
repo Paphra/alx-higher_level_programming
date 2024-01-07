77431fa1af69cd7392694d1c0c57bfeec0121d15
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-07 22:17:10 +0300

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""error_code module
+Sends a request to the passed URL
+"""
+
+import sys
+import requests
+
+
+def do_request(url):
+    """do_request funciton
+    Sends a request to the url
+    url (str): the URL
+    """
+
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print('Error code: {}'.format(res.status_code))
+    else:
+        print(res.content)
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    do_request(url)
