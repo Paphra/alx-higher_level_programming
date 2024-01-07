8ba4e5a87714502424463eae305f9f6b3220f539
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-07 08:32:26 +0300

    Changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,12 +11,13 @@ def fetch():
     Fetches the https://alx-intranet.hbtn.io/status
     """
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        data = res.read()
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
         print('Body response:')
-        print('    - type: {}'.format(type(data)))
-        print('    - content: {}'.format(data))
-        print('    - utf8 content: {}'.format(data.decode('utf-8')))
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode('utf-8')))
 
 
 if __name__ == '__main__':
