443f64eb67386c07a53e97f71257bda64d35827e
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-07 22:31:27 +0300

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""json_api module
+Contains code to send a POST request to url
+"""
+
+import sys
+import requests
+
+
+def do_request(url, letter):
+    """do_request function
+    Sends a POST request with a letter as a param
+    url (str): the URL
+    letter (str): the letter to send
+    """
+    if letter is None:
+        letter = ""
+    data = {
+        'q': letter
+    }
+    res = requests.post(url, data=data)
+    try:
+        json = res.json()
+        if len(json) > 0:
+            print('[{}] {}'.format())
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
+
+
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    letter = ""
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+
+    do_request(url, letter)
